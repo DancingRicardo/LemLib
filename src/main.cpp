394fd897805d09bb63f117e15ccecfa27ff2a1ab
DancingRicardo <99092565+DancingRicardo@users.noreply.github.com>
2024-01-31 08:17:48 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,63 +2,39 @@
 #include "lemlib/api.hpp"
 #include "lemlib/chassis/chassis.hpp"
 #include "lemlib/logger/stdout.hpp"
+#include "lemlib/pid.hpp"
+#include "pros/adi.hpp"
+#include "pros/colors.h"
 #include "pros/misc.h"
+#include "Graphy/Grapher.hpp"
 
 // controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
 // motor groups
 // left motors on ports 8, 20, and 19. Motors on ports 8 and 20 are reversed. Using blue gearbox
-auto leftMotors = lemlib::makeMotorGroup({-8, -20, 19}, pros::v5::MotorGears::blue);
+auto leftBottomMotors = lemlib::makeMotorGroup({-12, -13}, pros::v5::MotorGears::blue);
 // right motors on ports 2, 11, and 13. Motor on port 13 is reversed. Using blue gearbox
-auto rightMotors = lemlib::makeMotorGroup({2, 11, -13}, pros::v5::MotorGears::blue);
+auto rightBottomMotors = lemlib::makeMotorGroup({4, 3}, pros::v5::MotorGears::blue);
+
+// left motors on ports 8, 20, and 19. Motors on ports 8 and 20 are reversed. Using blue gearbox
+auto leftPTOMotors = lemlib::makeMotorGroup({11}, pros::v5::MotorGears::blue);
+// right motors on ports 2, 11, and 13. Motor on port 13 is reversed. Using blue gearbox
+auto rightPTOMotors = lemlib::makeMotorGroup({-1}, pros::v5::MotorGears::blue);
+
+pros::Motor intakeMotor(9);
+pros::Motor flywheelMotor(7);
+
+pros::adi::DigitalOut horizLeftFlap('A');
+pros::adi::DigitalOut horizRightFlap('B');
+pros::adi::DigitalOut vertLeftFlap('C');
+pros::adi::DigitalOut vertRightFlap('H');
+
+pros::adi::DigitalOut ptoLeft('E');
+pros::adi::DigitalOut ptoRight('F');
 
 // Inertial Sensor on port 11
-pros::Imu imu(11);
-
-// horizontal tracking wheel. Port 4, 2.75" diameter, 3.7" offset, back of the robot
-lemlib::TrackingWheel horizontal(4, lemlib::Omniwheel::NEW_275, -3.7);
-
-// drivetrain settings
-lemlib::Drivetrain drivetrain(leftMotors, // left motor group
-                              rightMotors, // right motor group
-                              10, // 10 inch track width
-                              lemlib::Omniwheel::NEW_325, // using new 3.25" omnis
-                              360, // drivetrain rpm is 360
-                              2 // chase power is 2. If we had traction wheels, it would have been 8
-);
-
-// linear motion controller
-lemlib::ControllerSettings linearController(10, // proportional gain (kP)
-                                            30, // derivative gain (kD)
-                                            1, // small error range, in inches
-                                            100, // small error range timeout, in milliseconds
-                                            3, // large error range, in inches
-                                            500, // large error range timeout, in milliseconds
-                                            20 // maximum acceleration (slew)
-);
-
-// angular motion controller
-lemlib::ControllerSettings angularController(2, // proportional gain (kP)
-                                             10, // derivative gain (kD)
-                                             1, // small error range, in degrees
-                                             100, // small error range timeout, in milliseconds
-                                             3, // large error range, in degrees
-                                             500, // large error range timeout, in milliseconds
-                                             20 // maximum acceleration (slew)
-);
-
-// sensors for odometry
-// note that in this example we use internal motor encoders, so we don't pass vertical tracking wheels
-lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to nullptr as we don't have one
-                            nullptr, // vertical tracking wheel 2, set to nullptr as we don't have one
-                            &horizontal, // horizontal tracking wheel 1
-                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
-                            &imu // inertial sensor
-);
-
-// create the chassis
-lemlib::Differential chassis(drivetrain, linearController, angularController, sensors);
+pros::Imu imu(10);
 
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -68,20 +44,7 @@ lemlib::Differential chassis(drivetrain, linearController, angularController, se
  */
 void initialize() {
     pros::lcd::initialize(); // initialize brain screen
-    chassis.initialize(); // calibrate sensors
-
-    // thread to for brain screen and position logging
-    pros::Task screenTask([&]() {
-        lemlib::Pose pose(0, 0, 0);
-        while (true) {
-            pose = chassis.getPose();
-            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
-            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
-            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
-            // delay to save resources
-            pros::delay(10);
-        }
-    });
+    imu.reset(true);
 }
 
 /**
@@ -98,29 +61,259 @@ void competition_initialize() {}
 // this needs to be put outside a function
 ASSET(example_txt); // '.' replaced with "_" to make c++ happy
 
+void turnTo(float degree) {
+
+    lemlib::FAPID turnPID(0, 0, 315, 0, 190, "Turn PID");
+
+    // Large error and small error are the ranges where the loop can exit. Small is the important one.
+    // Large and small times are for how long the bot must be within the range to exit. Max Time is
+    // the time it takes for the bot to give up on the PID loop and exit.
+    turnPID.setExit(1, .5, 900, 400, 3000);
+
+    float targetDistance = degree;
+
+    while (!turnPID.settled()) { // While the bot is still moving / oscillating
+
+        // Update the PID loop and get the motor voltage
+        int motorVoltage = turnPID.update(targetDistance, imu.get_yaw());
+
+        leftBottomMotors->move_voltage(motorVoltage);
+        rightBottomMotors->move_voltage(-motorVoltage);
+        leftPTOMotors->move_voltage(motorVoltage);
+        rightPTOMotors->move_voltage(-motorVoltage);
+
+        pros::delay(20);
+    }
+
+}
+
+void driveTo(float distance) {
+
+    graphy::AsyncGrapher grapher("Drive PID", 20);
+
+    lemlib::FAPID drivePID(0, 0, 400, 0, 0, "Drive PID");
+
+    // Large error and small error are the ranges where the loop can exit. Small is the important one.
+    // Large and small times are for how long the bot must be within the range to exit. Max Time is
+    // the time it takes for the bot to give up on the PID loop and exit.
+    drivePID.setExit(3, .1, 1000, 500, 5000);
+
+    float targetDistance = distance;
+
+    leftBottomMotors->set_zero_position_all(0);
+    rightBottomMotors->set_zero_position_all(0);
+    leftPTOMotors->set_zero_position_all(0);
+    rightPTOMotors->set_zero_position_all(0);
+
+    grapher.addDataType("Actual Distance", pros::c::COLOR_CYAN);
+    grapher.addDataType("Target Distance", pros::c::COLOR_RED);
+    grapher.addDataType("Voltage", pros::c::COLOR_YELLOW);
+
+    while (!drivePID.settled()) { // While the bot is still moving / oscillating
+
+        // Update the PID loop and get the motor voltage
+
+        float currentDistanceTraveled = ((leftBottomMotors->get_position(0) + rightBottomMotors->get_position()) / 2) *
+                                        3.14159265 / 180 * 1.625 * .75;
+
+        float motorVoltage = drivePID.update(targetDistance, currentDistanceTraveled);
+
+        if (motorVoltage > 12000) motorVoltage = 12000;
+
+        grapher.update("Actual Distance",
+                       currentDistanceTraveled / (targetDistance * 2));
+        grapher.update("Target Distance", (targetDistance / (targetDistance * 2)));
+        grapher.update("Voltage", (motorVoltage / 12000));
+
+        leftBottomMotors->move_voltage(motorVoltage);
+        rightBottomMotors->move_voltage(motorVoltage);
+        leftPTOMotors->move_voltage(motorVoltage);
+        rightPTOMotors->move_voltage(motorVoltage);
+
+        pros::delay(20);
+    }
+
+}
+
+
+void threeBall() {
+    /*======================== grab acorn under bar */
+
+    leftBottomMotors->move_voltage(3000);
+    rightBottomMotors->move_voltage(3000);
+    leftPTOMotors->move_voltage(3000);
+    rightPTOMotors->move_voltage(3000);
+
+    pros::delay(500);
+
+    leftBottomMotors->move_voltage(0);
+    rightBottomMotors->move_voltage(0);
+    leftPTOMotors->move_voltage(0);
+    rightPTOMotors->move_voltage(0);
+
+    /*======================== go backwards halfway*/
+
+    leftBottomMotors->move_voltage(-5500);
+    rightBottomMotors->move_voltage(-6000);
+    leftPTOMotors->move_voltage(-5500);
+    rightPTOMotors->move_voltage(-6000);
+
+    pros::delay(2000);
+
+    leftBottomMotors->move_voltage(0);
+    rightBottomMotors->move_voltage(0);
+    leftPTOMotors->move_voltage(0);
+    rightPTOMotors->move_voltage(0);
+
+    pros::delay(500);
+
+    /*========================*/
+
+    turnTo(-20);
+
+    /*========================*/
+
+    leftBottomMotors->move_voltage(-9000);
+    rightBottomMotors->move_voltage(-9000);
+    leftPTOMotors->move_voltage(-9000);
+    rightPTOMotors->move_voltage(-9000);
+
+    pros::delay(3000);
+
+    leftBottomMotors->move_voltage(0);
+    rightBottomMotors->move_voltage(0);
+    leftPTOMotors->move_voltage(0);
+    rightPTOMotors->move_voltage(0);
+}
+
+void sixBall() {
+
+    // Intake acorn under bar
+
+    driveTo(3);
+
+    // Back up
+
+    driveTo(-20);
+
+    // Turn to face goal
+
+    turnTo(-20);
+
+    // Drive to goal, extend back flap
+
+    driveTo(-10);
+
+    // Go out of goal, preparing to turn towards leftmost acorn
+
+    driveTo(5);
+    
+    // Turn towards leftmost acorn
+
+    turnTo(110);
+
+    // Drive to leftmost acorn, intake
+
+    intakeMotor.move(127);
+    driveTo(20);
+
+    // Turn towards goal, outtake
+
+    intakeMotor.move(0);
+    turnTo(35);
+    intakeMotor.move(-127);
+    pros::delay(500);
+    intakeMotor.move(0);
+
+    // Turn towards middle acorn by barrier
+
+    turnTo(-45);
+
+    // Drive to middle acorn, intake
+
+    intakeMotor.move(127);
+    driveTo(20);
+    intakeMotor.move(0);
+
+    // Turn towards goal
+
+    turnTo(90);
+
+    // Drive to goal, extend flaps, outtake
+
+    horizLeftFlap.set_value(true);
+    horizRightFlap.set_value(true);
+    driveTo(20);
+
+    // Retract flaps, back up so auton actually counts
+
+    horizLeftFlap.set_value(false);
+    horizRightFlap.set_value(false);
+    driveTo(-10);
+
+    
+
+}
+
 /**
  * Runs during auto
  *
  * This is an example autonomous routine which demonstrates a lot of the features LemLib has to offer
  */
 void autonomous() {
-    // example movement: Move to x: 20 and y:15, and face heading 90. Timeout set to 4000 ms
-    chassis.moveTo(20, 15, 90, 4000);
-    // example movement: Turn to face the point x:45, y:-45. Timeout set to 1000
-    // dont turn faster than 60 (out of a maximum of 127)
-    chassis.turnToPose(45, -45, 1000, true, 60);
-    // example movement: Follow the path in path.txt. Lookahead at 15, Timeout set to 4000
-    // following the path with the back of the robot (forwards = false)
-    // see line 116 to see how to define a path
-    chassis.follow(example_txt, 15, 4000, false);
-    // wait until the chassis has travelled 10 inches. Otherwise the code directly after
-    // the movement will run immediately
-    // Unless its another movement, in which case it will wait
-    chassis.waitUntil(10);
-    pros::lcd::print(4, "Travelled 10 inches during pure pursuit!");
-    // wait until the movement is done
-    chassis.waitUntilDone();
-    pros::lcd::print(4, "pure pursuit finished!");
+    graphy::AsyncGrapher grapher("Drive PID", 20);
+
+    lemlib::FAPID turnPID(0, 0, 315, 0, 190, "Turn PID");
+    lemlib::FAPID drivePID(0, 0, 950, 0, 2000, "Drive PID");
+
+    // Large error and small error are the ranges where the loop can exit. Small is the important one.
+    // Large and small times are for how long the bot must be within the range to exit. Max Time is
+    // the time it takes for the bot to give up on the PID loop and exit.
+    turnPID.setExit(1, .5, 900, 400, 3000);
+    drivePID.setExit(3, .1, 1000, 500, 5000);
+
+    float targetDistance = 48.f;
+
+    leftBottomMotors->set_zero_position_all(0);
+    rightBottomMotors->set_zero_position_all(0);
+    leftPTOMotors->set_zero_position_all(0);
+    rightPTOMotors->set_zero_position_all(0);
+
+    grapher.addDataType("Actual Distance", pros::c::COLOR_CYAN);
+    grapher.addDataType("Target Distance", pros::c::COLOR_RED);
+    grapher.addDataType("Voltage", pros::c::COLOR_YELLOW);
+
+    grapher.startTask();
+
+    while (!drivePID.settled()) { // While the bot is still moving / oscillating
+
+        // Update the PID loop and get the motor voltage
+
+        float currentDistanceTraveled = ((leftBottomMotors->get_position(0) + rightBottomMotors->get_position()) / 2) *
+                                        3.14159265 / 180 * 1.625 * .75;
+
+        float motorVoltage = drivePID.update(targetDistance, currentDistanceTraveled);
+
+        if (motorVoltage > 12000) motorVoltage = 12000;
+
+        grapher.update("Actual Distance",
+                       currentDistanceTraveled / (targetDistance * 2));
+        grapher.update("Target Distance", (targetDistance / (targetDistance * 2)));
+        grapher.update("Voltage", (motorVoltage / 12000));
+
+        leftBottomMotors->move_voltage(motorVoltage);
+        rightBottomMotors->move_voltage(motorVoltage);
+        leftPTOMotors->move_voltage(motorVoltage);
+        rightPTOMotors->move_voltage(motorVoltage);
+
+        pros::delay(20);
+    }
+
+    while (1) {
+
+    }
+
+    // threeBall();
 }
 
 /**
@@ -129,13 +322,83 @@ void autonomous() {
 void opcontrol() {
     // controller
     // loop to continuously update motors
+
+    bool isLifted = false;
+
     while (true) {
+
+        /*=================*/
+
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
+            horizLeftFlap.set_value(true);
+            horizRightFlap.set_value(true);
+        } else {
+            horizLeftFlap.set_value(false);
+            horizRightFlap.set_value(false);
+        }
+
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L2)) {
+            vertLeftFlap.set_value(true);
+            vertRightFlap.set_value(true);
+        } else {
+            vertLeftFlap.set_value(false);
+            vertRightFlap.set_value(false);
+        }
+
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
+            intakeMotor.move_velocity(12000);
+        } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2)) {
+            intakeMotor.move_velocity(-12000);
+        } else {
+            intakeMotor.move_velocity(0);
+        }
+
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_A)) {
+            flywheelMotor.move_velocity(12000);
+        } else {
+            flywheelMotor.move_velocity(0);
+        }
+
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)) {
+            ptoLeft.set_value(false);
+            ptoRight.set_value(false);
+            isLifted = true;
+        }   
+        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
+            ptoLeft.set_value(true);
+            ptoRight.set_value(true);
+            isLifted = false;
+        }
+
+
         // get joystick positions
         int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-        // move the chassis with curvature drive
-        chassis.curvature(leftY, rightX);
-        // delay to save resources
-        pros::delay(10);
+        int rightY = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+        
+        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_UP) && isLifted) {
+            leftPTOMotors->move(-127);
+            rightPTOMotors->move(-127);
+        } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN) && isLifted) {
+            leftPTOMotors->move(127);
+            rightPTOMotors->move(127);
+        }
+        else if (!isLifted) {
+            leftPTOMotors->move(leftY);
+            rightPTOMotors->move(rightY);
+        }
+        else {
+            leftPTOMotors->move(0);
+            rightPTOMotors->move(0);
+        
+        }
+
+
+        /*===== DRIVE =====*/
+
+        leftBottomMotors->move(leftY);
+        rightBottomMotors->move(rightY);
+
+
+        pros::delay(20);
     }
 }
